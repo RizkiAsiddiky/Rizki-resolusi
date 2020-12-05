567e5b4900c575ee93eddcc2efc29db2c110feda
RizkiAsiddiky <75513348+RizkiAsiddiky@users.noreply.github.com>
2020-12-05 12:33:02 +0800

    VARIABEL BANYAK KATA.cpp

---
+++ explore variabel bnyak kata.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string aktivitas;
+	string tempat;
+	
+	
+	cout<<"apa aktivitas anda \t: ";
+	cin>>aktivitas;
+	
+	cout<<"dimana anda "<<aktivitas <<" \t: ";
+	cin>>tempat;
+	
+	
+	cout<<"saya sedang "<<aktivitas<<" di "<<tempat<<endl;
+	
+}
